8b4a6d2e26f68b84f0adc6fa80d9a7f12eda0c69
satyamnarayan14 <satyamnarayan14@gmail.com>
2025-09-25 15:40:49 +0000

    datatypes and typeof function and null

---
+++ satyam/01_basic_variable.js
@@ -0,0 +1,20 @@
+const accountId=14453
+let accountEmail="satyam@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState
+//accountId=2
+accountEmail="satym@gmail.com"
+accountPassword="20849"
+accountCity="patna"
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity,accountState])
+
+{
+    //scope bol jata bhaiyo
+}
+/*
+prefer not to use var 
+because of issue in block scope and fundtional scope
+*/
---
+++ satyam/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";
+//treat all js code as newer version
+// alert("hello")
+// alert(3+2)
+// we are useing nodejs not browser
+console.log(3+2); console.log("hitesh")
+// we have maintain code readability
+
+let name="hitesh"
+let age=18
+let isLoggedIn=false
+let state;
+
+
+//number=> 2 to power 53
+//bigInt
+//string=>""
+//boolean=>true/false
+//null =>standalone value
+//undefined =>
+//symbol=>unique
+//object
+console.log(typeof(age))
+console.log(typeof("satyam"))
+console.log(typeof(null))//object
+console.log(typeof(undefined))//undefined
